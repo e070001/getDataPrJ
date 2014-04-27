f5708c4de0b8d608119db875f75892cfebe614e9
e070001 <e070001@e.ntu.edu.sg>
2014-04-27 21:08:05 +0800

    GetDataPrJ

--- run_analysis.R
+++ run_analysis.R
@@ -12,7 +12,7 @@ path_prefix <- "test"
 fname_suffix <- "test"
 
 # read the Y Test data file
-fpath <- file.path(path_prefix paste0("y_", fname_suffix, ".txt"))      
+fpath <- file.path(path_prefix, paste0("y_", fname_suffix, ".txt"))      
 y_data <- read.table(fpath, header=F, col.names=c("ActivityID"))
 
 # read the subject data file
@@ -42,7 +42,7 @@ path_prefix <- "train"
 fname_suffix <- "train"
 
 # read the Y Test data file
-fpath <- file.path(path_prefix paste0("y_", fname_suffix, ".txt"))      
+fpath <- file.path(path_prefix, paste0("y_", fname_suffix, ".txt"))      
 y_data <- read.table(fpath, header=F, col.names=c("ActivityID"))
 
 # read the subject data file
@@ -66,17 +66,36 @@ data$SubjectID <- subject_data$SubjectID
 # record the test data
 train_data <- data
 
+## Step 2. Merging both training and test data sets with column names
+data <- rbind(test_data, train_data)
+cnames <- colnames(data)
+cnames <- gsub("\\.+mean\\.+", cnames, replacement="Mean")
+cnames <- gsub("\\.+std\\.+",  cnames, replacement="Std")
+colnames(data) <- cnames
 
-
-
-
-
-print("STEP 1: Merging the training and the test sets to create one data set...")
-activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt", 
-                              header=F, 
-                              as.is=T, 
-                              col.names=c("ActivityID",
-                                          "ActivityName"))
+## Step 3. Add the activity names as another column
+activity_labels <- read.table("activity_labels.txt", header=F, as.is=T, col.names=c("ActivityID", "ActivityName"))
 activity_labels$ActivityName <- as.factor(activity_labels$ActivityName)
-data_labeled <- merge(data, activity_labels)
-data_labeled
+data_labeled <- merge(data, activity_labels) # Merged Labeled Data is obtained
+merged_labeled_data <- data_labeled
+
+## Step 4. Create a tidy data set that has the average of each variable for each activity and each subject.
+library(reshape2)
+
+# melt the dataset
+id_vars = c("ActivityID", "ActivityName", "SubjectID")
+measure_vars = setdiff(colnames(merged_labeled_data), id_vars)
+melted_data <- melt(merged_labeled_data, id=id_vars, measure.vars=measure_vars)
+
+# recast 
+tidy_data <- dcast(melted_data, ActivityName + SubjectID ~ variable, mean)  
+
+# save the tidy data set into a named file
+fname <- "tidy_data_set.txt"
+write.table(tidy_data, fname)
+
+
+
+
+
+
